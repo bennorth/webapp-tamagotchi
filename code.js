540196d8b507d96b4b87d19b40849f3cbd283777
Ben North <ben@redfrontdoor.org>
2016-01-19 21:46:07 +0000

    Use draw_circle() to draw the egg
    This makes it clearer what we're doing.

--- code.js
+++ code.js
@@ -11,9 +11,7 @@ $(document).ready(function()
     }
 
     ctx.fillStyle = 'green';
-    ctx.beginPath();
-    ctx.arc(240, 240, 180, 0.0, 2.0 * Math.PI, false);
-    ctx.fill();
+    draw_circle(240, 240, 180);
 
     function draw_alien()
     {
