8944db3c2a63b7d12a4e6adc61f512e935acd249
Ben North <ben@redfrontdoor.org>
2016-01-19 21:58:13 +0000

    Fade in levels display after 'hatch' faded out

--- code.js
+++ code.js
@@ -26,7 +26,7 @@ $(document).ready(function()
 
     function fade_out_hatch_instructions()
     {
-        $('#hatch-instructions').fadeOut();
+        $('#hatch-instructions').fadeOut(400, fade_in_levels_display);
     }
 
     function fade_in_levels_display()
