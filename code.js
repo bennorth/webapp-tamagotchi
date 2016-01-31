737f5ed414d0c545fb876e268371f8fa0b9e5eba
Ben North <ben@redfrontdoor.org>
2016-01-31 14:01:24 +0000

    Connect 'sweets' button to feed_alien_sweets()
    This is familiar by now &mdash; declare that we want the
    `feed_alien_sweets` function to be run when the '*Sweets*' button is
    clicked.

--- code.js
+++ code.js
@@ -78,6 +78,9 @@ $(document).ready(function()
 
         $('#hungriness').html(hungriness);
     }
+
+    $('#feed-sweets').click(feed_alien_sweets);
+
     function time_goes_by()
     {
         make_alien_hungrier();
