1963e105ce9785fdcf34564177b2abd723084d6c
Ben North <ben@redfrontdoor.org>
2016-06-18 08:19:12 +0100

    draw_alien(): Add
    To actually draw the alien, we'll just use two overlapping blue circles.
    To say where the circles go, we use coordinates as before.  Now it
    should actually draw the Tamagotchi when you click the 'hatch' button.

--- code.js
+++ code.js
@@ -8,5 +8,19 @@ $(document).ready(function()
     ctx.arc(240, 240, 180, 0.0, 2.0 * Math.PI, false);
     ctx.fill();
 
+    function draw_alien()
+    {
+        ctx.clearRect(0, 0, 480, 480);
+        ctx.fillStyle = 'blue';
+
+        ctx.beginPath();
+        ctx.arc(240, 110, 80, 0.0, 2.0 * Math.PI, false);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.arc(240, 300, 150, 0.0, 2.0 * Math.PI, false);
+        ctx.fill();
+    }
+
     $('#hatch').click(draw_alien);
 });
